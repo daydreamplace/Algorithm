1b2b51d0b1e32ca793a4fcb2a6321b1a3723bcd3
Eden <109200582+daydreamplace@users.noreply.github.com>
2024-11-11 11:15:15 +0900

    [level 1] Title: 내적, Time: 0.03 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var sum = 0
-    
-    for i in 0..<a.count {
-        sum += a[i] * b[i]
-    }
-    
-    return sum
+    return 1234567890
 }
