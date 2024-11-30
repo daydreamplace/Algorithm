af2c5bf0400441c0a8d49cce468905e37dee4a5a
Eden <you8inpark@gmail.com>
2024-11-30 22:37:57 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int, _ numlist:[Int]) -> [Int] {
+    var array: [Int] = []
+    for i in numlist {
+        if i % n == 0 {
+            array.append(i)
+        }
+    }
+    return array
+}
