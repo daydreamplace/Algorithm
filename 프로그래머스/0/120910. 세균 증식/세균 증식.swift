77b1c09bad2bb42664b008bbca2b39c3964878eb
Eden <you8inpark@gmail.com>
2025-03-17 17:55:01 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.swift
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 func solution(_ n:Int, _ t:Int) -> Int {
+    var sum = n
+    var count = 0
 
-    return n * Int(pow(2.0, Double(t)))
+    for i in 0...t {
+        if t < i {
+            break
+        }
+        sum += 2 * n
+        count += 1
+    }
+    return sum
 }
