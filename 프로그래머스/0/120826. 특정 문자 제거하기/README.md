80ea2b5aa3a983e04de99b4189a21c7478aa1182
Eden <you8inpark@gmail.com>
2025-03-18 12:19:10 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/README.md
+++ 프로그래머스/0/120826. 특정 문자 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자 제거하기 - 120826 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 0.24 ms
+메모리: 16.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 07일 18:52:37
+2025년 03월 18일 12:19:08
 
 ### 문제 설명
 
--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 func solution(_ my_string:String, _ letter:String) -> String {
-    return my_string.replacingOccurrences(of: letter, with: "")
+    var string_arr: [String] = my_string.map { String($0) }
+    var letter_arr: [String] = letter.map { String($0) }
+    var arr: [String] = []
+    
+    for char in string_arr {
+        for j in letter_arr {
+            if char != j {
+                arr.append(char)
+            }
+        }
+    }
+    var string = arr.joined()
+    return string
 }
