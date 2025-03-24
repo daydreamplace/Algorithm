331f5bc7ad89eed453752403b48e98f4893150ef
Eden <you8inpark@gmail.com>
2025-03-24 21:48:36 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -1,15 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var count = 0
-    if n > 7 {
-        if n % 7 == 0 {
-            count = n / 7
-        } else {
-            count = n / 7 + 1
-        }
-    } else {
-        count = 1
-    }
-    return count
+    return n % 7 != 0 ? n / 7 + 1 : n / 7
 }
