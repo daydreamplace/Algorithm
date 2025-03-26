87dad918967c347443de1f3e2feaa72daa4af363
Eden <you8inpark@gmail.com>
2025-03-26 15:28:12 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    var arr: [Int] = []
-    for i in num1...num2 {
-        let nums = numbers
-        arr.append(nums[i])
-    }
-    return arr
+    return Array(numbers[num1...num2])
 }
