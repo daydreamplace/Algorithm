cf742fb1ec99bf54a5eaea211ab6d929e52ec572
Eden <you8inpark@gmail.com>
2025-03-30 23:09:58 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.swift
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -1,15 +1,16 @@
 import Foundation
 
 func solution(_ dot:[Int]) -> Int {
-    if dot[0] > 0 && dot[1] > 0 {
+    let x = dot[0]
+    let y = dot[1]
+    
+    if x > 0 && y > 0 {
         return 1
-    } else if dot[0] < 0 && dot[1] < 0 {
-        return 3
-    } else if dot[0] < 0 && dot[1] > 0 {
+    } else if x < 0 && y > 0 {
         return 2
-    } else if dot[0] > 0 && dot[1] < 0 {
-        return 4
+    } else if x < 0 && y < 0 {
+        return 3
     } else {
-        return 0
+        return 4
     }
 }
