4e1d4a1d273f59d2ea64e3efbffacbf191c52d8b
Eden <you8inpark@gmail.com>
2025-03-31 16:00:39 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    let array = numbers.sorted(by: <)
-    return array[numbers.count-1] * array[numbers.count-2]
+    let arr = numbers.sorted(by: <)
+    
+    return arr[arr.count - 1] * arr[arr.count - 2]
 }
