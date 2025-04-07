141856a551de13fb4b30ed9d69f7e14d1295a40d
Eden <you8inpark@gmail.com>
2025-04-08 00:41:23 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.swift
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 func solution(_ s1:[String], _ s2:[String]) -> Int {
-    var count = 0
-    
-    for i in s1 {
-        for j in s2 {
-            if i == j {
-                count += 1
+    var count: Int = 0
+    for char in s1 {
+        for char2 in s2 {
+            
+            if char == char2 {
+                count += 1 
             }
         }
     }
